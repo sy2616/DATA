8d2314b23c9757c8f54a85a01b451aaa29054912
sy2616 <cqj8685123@gmail.com>
2021-02-25 16:52:55 +0800

    33

--- 压制.py
+++ 压制.py
@@ -1,5 +1,15 @@
 import os
-path=r'"C:\Users\Jack\Downloads\HUB\Amateur Japanese Teen with Tight Pussy Gets Fucked - Pornhub.com.ts"'
-out=r'D:\1.mp4'
-cmd='ffmpeg -i %s -t 60 -c:v libx265 -crf 20 -c:a copy %s'%(path,out)
-os.system(cmd)
+#path=r'"C:\Users\Jack\Downloads\HUB\Amateur Japanese Teen with Tight Pussy Gets Fucked - Pornhub.com.ts"'
+#out=r'D:\1.mp4'
+#cmd='ffmpeg -i %s -t 60 -c:v libx265 -crf 20 -c:a copy %s'%(path,out)
+#os.system(cmd)
+path=r'C:\Users\Jack\Downloads'
+for root,dirs,files in os.walk(path):
+    for file in files:
+        print(os.path.abspath(file))
+        if os.path.splitext(file)[1] =='.ts':
+            oldfile=os.path.join(root,file)
+            newfile=os.path.join(path,file)
+            os.renames(oldfile,newfile)
+            print('{} copy to new file'.format(file))
+ 
--- 批量删除.py
+++ 批量删除.py
@@ -1,6 +1,24 @@
 import os
-dd=r'D:'
+dd=r'E:'
+# n=0
 for root,dirs,files in os.walk(dd):
-    print(root)
-    print(dirs)
-    print(files)
+    # print(root)
+    # print(dirs)
+    # print(files)
+    for file in files:
+        if file == '有趣的小视频.mp4':
+            os.remove(os.path.join(root,file))
+            n+=1
+            print('remove successes')
+        if os.path.splitext(file)[1]=='.bc!':
+            os.remove(os.path.join(root,file))    #删除特定格式文件
+            print('remove bc!')
+    for dir in dirs:
+        nn=os.path.join(root,dir)
+        try:
+            os.rmdir(nn)     #删除空文件夹
+            print('remove %s'%nn)
+        except:
+            pass
+
+# print('%s are removed'%n)
